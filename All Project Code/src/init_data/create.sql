7aae1a3d0cace0a9a142f22deab7996ecdf0bce8
Ojas Baral <ojas.baral@gmail.com>
2023-11-09 22:11:02 -0700

    Fixed .gitignore and added create and insert scripts for db

--- All Project Code/src/init_data/create.sql
+++ All Project Code/src/init_data/create.sql
@@ -8,15 +8,16 @@ CREATE TABLE users(
 	contact_info VARCHAR(100),
 	rate_info VARCHAR(100),
 	tutor BOOLEAN DEFAULT false,
-	student BOOLEAN DEFAULT false,	
+	student BOOLEAN DEFAULT false
 );
 
 DROP TABLE IF EXISTS posts CASCADE;
 CREATE TABLE posts(
 	post_id SERIAL primary key,
 	user_id inT not null,
-	post varchar(200)
-	FOREIGN KEY (user_id) REFERENCES user(user_id)
+    upvote int default 0,
+	post varchar(200),
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 DROP TABLE IF EXISTS user_to_user CASCADE;
@@ -25,21 +26,22 @@ CREATE TABLE user_to_user(
 	student_user serial,
 	PRIMARY KEY (tutor_user, student_user),
 	FOREIGN KEY (tutor_user) REFERENCES users(user_id),
-	FOREIGN KEY (student_user) REFERENCES users(user_id),
+	FOREIGN KEY (student_user) REFERENCES users(user_id)
 );
 
 DROP TABLE IF EXISTS courses CASCADE;
 CREATE TABLE courses(
-    course_id NUMERIC PRIMARY KEY,
+    course_id SERIAL PRIMARY KEY,
     course_name VARCHAR(100) NOT NULL,
     credit_hours NUMERIC NOT NULL
 );
 
-DROP TABLE IF EXISTS users_to_classes CASCADE;
-CREATE TABLE users_to_classes(
+DROP TABLE IF EXISTS users_to_courses CASCADE;
+CREATE TABLE users_to_courses(
 	user_id int,
 	course_id int,
 	tutoring boolean default false,
+    PRIMARY KEY (user_id, course_id),
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
 	FOREIGN KEY (course_id) REFERENCES courses(course_id)
 );
