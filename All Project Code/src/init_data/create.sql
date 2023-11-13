51d104642b64987a7b5c934735048efecce913f2
Ojas Baral <ojas.baral@gmail.com>
2023-11-12 20:21:40 -0700

    Fixed logical error in create.sql, Edited create.sql to follow convention

--- All Project Code/src/init_data/create.sql
+++ All Project Code/src/init_data/create.sql
@@ -13,17 +13,17 @@ CREATE TABLE users(
 
 DROP TABLE IF EXISTS posts CASCADE;
 CREATE TABLE posts(
-	post_id SERIAL primary key,
-	user_id inT not null,
-    upvote int default 0,
-	post varchar(200),
+	post_id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+    upvote INT DEFAULT 0,
+	post VARCHAR(200),
 	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 DROP TABLE IF EXISTS user_to_user CASCADE;
 CREATE TABLE user_to_user(
-	tutor_user serial,
-	student_user serial,
+	tutor_user INT,
+	student_user INT,
 	PRIMARY KEY (tutor_user, student_user),
 	FOREIGN KEY (tutor_user) REFERENCES users(user_id),
 	FOREIGN KEY (student_user) REFERENCES users(user_id)
@@ -38,9 +38,9 @@ CREATE TABLE courses(
 
 DROP TABLE IF EXISTS users_to_courses CASCADE;
 CREATE TABLE users_to_courses(
-	user_id int,
-	course_id int,
-	tutoring boolean default false,
+	user_id INT,
+	course_id INT,
+	tutoring BOOLEAN DEFAULT false,
     PRIMARY KEY (user_id, course_id),
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
 	FOREIGN KEY (course_id) REFERENCES courses(course_id)
