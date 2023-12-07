5b8206f791b929cd574dc460a4b1352e202f19a9
Ojas Baral <ojas.baral@gmail.com>
2023-12-07 13:39:52 -0700

    fixing courses table

--- All Project Code/components/src/init_data/create.sql
+++ All Project Code/components/src/init_data/create.sql
@@ -32,7 +32,7 @@ CREATE TABLE user_to_user(
 
 DROP TABLE IF EXISTS courses CASCADE;
 CREATE TABLE courses(
-    course_id SERIAL PRIMARY KEY,
+    course_id INT PRIMARY KEY,
     course_name VARCHAR(100) NOT NULL,
     credit_hours NUMERIC NOT NULL
 );
